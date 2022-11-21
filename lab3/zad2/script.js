3dd7e425d6170182a9b2b4e7cc0d40e7ca4ccd60
Czajor <mczajor@student.agh.edu.pl>
2022-11-21 15:09:18 +0100

    changes

---
+++ lab3/zad2/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn1").onclick = function(){
+    if (document.getElementById("hide").style.display=="block"){
+        document.getElementById("hide").style.display="none";
+    } else {
+        document.getElementById("hide").style.display="block";
+    }
+};
