8d5b01e6f22b934424a70171431f172ffea8b4c9
mczajor <mczajor@student.agh.edu.pl>
2022-11-28 10:55:01 +0100

    finished zad10

---
+++ lab3/zad10/script.js
@@ -0,0 +1,39 @@
+var box = document.querySelector("#box");
+var circle = document.querySelector("#circle");
+var body = document.querySelector("body");
+var clicked = false;
+
+box.addEventListener("click", changePosition, false);
+body.addEventListener("click", displayMessage, false);
+
+function changePosition(e) {
+    clicked = true;
+    var boxPosition = getPosition(box);
+    var yPosition = e.clientY - (circle.offsetHeight/2) - boxPosition.y;
+    var xPosition = e.clientX - (circle.offsetWidth/2) - boxPosition.x;
+    circle.style.transform = "translate3d(" + xPosition + "px, " + yPosition + "px, 0)";
+}
+
+
+function getPosition(element){
+    var xPosition = 0;
+    var yPosition = 0;
+    while (element){
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+    return {x: xPosition, y: yPosition};
+}
+
+function displayMessage(){
+    console.log("body clicked");
+    var message = document.querySelector("#message");
+    if(clicked){
+        message.textContent = "";
+        clicked = false;
+        return;
+    } else {
+        message.textContent = "Naciśnij wewnątrz pudełka";
+    }
+}
